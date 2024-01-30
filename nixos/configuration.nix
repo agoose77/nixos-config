d04de965553c800cc5b5b19ae451928605a18e2c
Angus Hollands <goosey15@gmail.com>
2024-01-30 23:29:02 +0000

    wip

---
+++ nixos/configuration.nix
@@ -0,0 +1,164 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other NixOS modules here
+  imports = [
+    # If you want to use modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+    ./hardware-configuration.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  # This will add each flake input as a registry
+  # To make nix3 commands consistent with your flake
+  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+
+  # This will additionally add your inputs to the system's legacy channels
+  # Making legacy nix commands consistent as well, awesome!
+  nix.nixPath = ["/etc/nix/path"];
+  environment.etc =
+    lib.mapAttrs'
+    (name: value: {
+      name = "nix/path/${name}";
+      value.source = value.flake;
+    })
+    config.nix.registry;
+
+  nix.settings = {
+    # Enable flakes and new 'nix' command
+    experimental-features = "nix-command flakes";
+    # Deduplicate and optimize nix store
+    auto-optimise-store = true;
+  };
+
+  # FIXME: Add the rest of your current configuration
+
+  # TODO: Set your hostname
+  networking.hostName = "angus";
+  networking.networkManager.enable = true;
+
+  time.timeZone = "Europe/London";
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  # TODO: i18n for lC_
+
+  services.xserver = {
+  	layout = "gb";
+  	xkbVariant = "";
+  };
+
+  console.keyMap = "uk";
+
+  # TODO: This is just an example, be sure to use whatever bootloader you prefer
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;  
+
+  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
+  users.users = {
+    # FIXME: Replace with your username
+    angus = {
+      isNormalUser = true;
+      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+      extraGroups = [ "networkmanager", "wheel" ];
+      packages = with pkgs; [];
+    };
+  };
+
+  # This setups a SSH server. Very important if you're setting up a headless system.
+  # Feel free to remove if you don't need it.
+  services.openssh = {
+    enable = true;
+    settings = {
+      # Forbid root login through SSH.
+      PermitRootLogin = "no";
+      # Use keys only. Remove if you want to SSH using password (not recommended)
+      PasswordAuthentication = false;
+    };
+  };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.05";
+
+  environment.systemPackages = with pkgs; [
+  	micro
+  	alacritty
+  	firefox
+  	kitty
+  	git
+  ];
+
+  environment.variables.EDITOR = "micro";
+
+  environment.sessionVariables = rec {
+  	WLR_NO_HARDWARE_CURSORS = "1";
+  };
+
+  programs.hyprland = {
+	enable = true;
+	xwayland.enable = true;
+	nvidiaPatches = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl = {
+  	enable = true;
+  	driSupport = true;
+  	driSupport32Bit = true;
+  };
+
+  hardware.nvidia = {
+  	modesetting.enable = true;
+  	open = true;
+  	nvidiaSettings = true;
+  	package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  sound.enable = true;
+  services = {
+  	pipewire = {
+  		enable = true;
+  		audio.enable = true;
+  		pulse.enable = true;
+  		alsa = {
+  			enable = true;
+  			support32Bit = true;
+  		};
+  		jack.enable = true;
+  	};
+  };
+
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.enable = true;
+}
