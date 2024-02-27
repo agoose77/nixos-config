34e89d4d5ab67825c735633ca8938e034b4e0635
Angus Hollands <goosey15@gmail.com>
2024-02-27 09:59:01 +0000

    wip: lsp keymaps

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -225,6 +225,21 @@
       telescope.enable = true;
       lsp = {
         enable = true;
+	keymaps = {
+  	  silent = true;
+	  diagnostic = {
+	    "<leader>k" = "goto_prev";
+  	    "<leader>j" = "goto_next";
+	  };
+          lspBuf = {
+            gd = "definition";
+            gD = "references";
+            gt = "type_definition";
+            gi = "implementation";
+            K = "hover";
+            "<F2>" = "rename";
+          };
+	};
         servers = {
           pylsp.enable = true;
 	  ruff-lsp.enable = true;
