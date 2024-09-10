ce138f6147d0cad65058ca6655c7954c0b73a498
Angus Hollands <goosey15@gmail.com>
2024-09-10 09:45:53 +0100

    feat: more nixvim support

--- home/angus/global/common/nixvim.nix
+++ home/angus/global/common/nixvim.nix
@@ -8,10 +8,23 @@
     enable = true;
     defaultEditor = true;
     plugins = {
+      cmp.enable = true;
+      #        cmp.mapping.preset.insert({
+      #    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      #    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      #    ['<C-Space>'] = cmp.mapping.complete(),
+      #    ['<C-e>'] = cmp.mapping.abort(),
+      #    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      #  })
       fugitive.enable = true;
       gitsigns.enable = true;
       telescope.enable = true;
-      cmp.enable = true;
+
+      treesitter.enable = true;
+      treesitter-context.enable = true;
+
+      # treesitter-textobjects.enable = true; TODO: this would be cool
+      lsp-format.enable = true;
       lsp = {
         enable = true;
         keymaps = {
@@ -32,8 +45,25 @@
         servers = {
           pylsp.enable = true;
           ruff-lsp.enable = true;
-          tsserver.enable = true;
+          eslint.enable = true;
           nil-ls.enable = true;
+        };
+      };
+      oil = {
+        enable = true;
+        settings = {
+          # Don't perm-delete files first
+          delete_to_trash = true;
+          use_default_keymaps = true;
+          # Use LSP to aut-change names of files we modify
+          lsp_file_method.autosave_changes = true;
+          buf_options = {
+            buflisted = true;
+            bufhidden = "hide";
+          };
+          view_options = {
+            show_hidden = true;
+          };
         };
       };
     };
