3fc6febf0f9a5ecc9c118a15385b9cd8cfeb0395
Angus Hollands <goosey15@gmail.com>
2025-03-17 11:01:05 +0000

    feat: add hypridle

--- home/angus/global/common/hyprland.nix
+++ home/angus/global/common/hyprland.nix
@@ -18,6 +18,22 @@
     configPackages = [pkgs.hyprland];
   };
 
+  home.file.".config/hypr/hypridle.conf".text = ''
+    general {
+        lock_cmd = notify-send "lock!"          # dbus/sysd lock command (loginctl lock-session)
+        unlock_cmd = notify-send "unlock!"      # same as above, but unlock
+        before_sleep_cmd = notify-send "Zzz"    # command ran before sleep
+        after_sleep_cmd = notify-send "Awake!"  # command ran after sleep
+        ignore_dbus_inhibit = false             # whether to ignore dbus-sent idle-inhibit requests (used by e.g. firefox or steam)
+        ignore_systemd_inhibit = false          # whether to ignore systemd-inhibit --what=idle inhibitors
+    }
+
+    listener {
+        timeout = 300                            # in seconds
+        on-timeout = notify-send "You are idle!" # command to run when timeout has passed
+        on-resume = notify-send "Welcome back!"  # command to run when activity is detected after timeout has fired.
+    }  '';
+
   wayland.windowManager.hyprland = {
     enable = true;
     extraConfig = ''
@@ -41,7 +57,7 @@
         "element-desktop --hidden"
         "zulip"
         "kdeconnect-indicator"
-	"hypridle"
+        "hypridle"
         # Waybar appears to start itself
         # "waybar"
       ];
