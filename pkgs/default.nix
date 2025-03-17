d80f3d41a1a12e8a987d579611e8dd95af7eb89c
Angus Hollands <goosey15@gmail.com>
2025-03-17 11:00:33 +0000

    refactor: move nvidia-offload to its own package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,10 +1,4 @@
 pkgs: {
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec "$@"
-  '';
+  nvidia-offload = pkgs.callPackage ./nvidia-offload {};
   idle-monitor = pkgs.callPackage ./idle-monitor {};
 }
