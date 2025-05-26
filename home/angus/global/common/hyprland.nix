d5df9d6df036ec6ba8896c31955196f3e87e6f5e
Angus Hollands <goosey15@gmail.com>
2025-05-26 11:36:15 +0100

    wip monitors

--- home/angus/global/common/hyprland.nix
+++ home/angus/global/common/hyprland.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: {
   xdg.mimeApps.enable = true;
   home.packages = with pkgs; [
     wf-recorder
@@ -44,7 +49,44 @@
         no_hardware_cursors = true;
       };
       # See https://wiki.hyprland.org/Configuring/Monitors/
-      monitor = ",preferred,auto,1";
+      monitor = let
+        waybarSpace = let
+          inherit (config.wayland.windowManager.hyprland.settings.general) gaps_in gaps_out;
+          inherit (config.programs.waybar.settings.primary) position height width;
+          gap = gaps_out - gaps_in;
+        in {
+          top =
+            if (position == "top")
+            then height + gap
+            else 0;
+          bottom =
+            if (position == "bottom")
+            then height + gap
+            else 0;
+          left =
+            if (position == "left")
+            then width + gap
+            else 0;
+          right =
+            if (position == "right")
+            then width + gap
+            else 0;
+        };
+      in
+        [
+          ",addreserved,${toString waybarSpace.top},${toString waybarSpace.bottom},${toString waybarSpace.left},${toString waybarSpace.right}"
+        ]
+        ++ (map (
+          m: "${m.name},${
+            if m.enabled
+            then "${toString m.width}x${toString m.height}@${toString m.refreshRate},${m.position},${m.scale}"
+            else "disable"
+          }"
+        ) (config.monitors));
+
+      workspace = map (m: "${m.workspace},monitor:${m.name}") (
+        lib.filter (m: m.enabled && m.workspace != null) config.monitors
+      );
       # See https://wiki.hyprland.org/Configuring/Keywords/ for more
 
       # Execute your favorite apps at launch
