6bba7674aef71286ab673791e7c6391c8ad709b5
Angus Hollands <goosey15@gmail.com>
2025-05-26 11:51:59 +0100

    fix: waldo monitor names

--- home/angus/waldo.nix
+++ home/angus/waldo.nix
@@ -2,14 +2,14 @@
   imports = [./global];
   monitors = [
     {
-      name = "DP-1";
+      name = "eDP-1";
       width = 1920;
       height = 1200;
       workspace = "1";
       primary = true;
     }
     {
-      name = "DP-2";
+      name = "eDP-2";
       width = 1920;
       height = 1200;
       position = "auto-down";
