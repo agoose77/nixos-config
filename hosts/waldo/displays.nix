929a6fc77e50fa230554eee93291d3d742201974
Angus Hollands <goosey15@gmail.com>
2025-05-26 15:53:38 +0100

    wip: fix udev rules

--- hosts/waldo/displays.nix
+++ hosts/waldo/displays.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   # Disable eDP-2 when keyboard plugged in
   services.udev.extraRules = ''
-    ACTION==""add"",    ATTRS{idVendor}==""0b05"", ATTRS{idProduct}==""1b2c"", RUN+=""${pkgs.duo-display} top""
-    ACTION==""remove"", ATTRS{idVendor}==""0b05"", ATTRS{idProduct}==""1b2c"", RUN+=""${pkgs.duo-display} both""
+    ACTION=="add",    ATTRS{idVendor}=="0b05", ATTRS{idProduct}=="1b2c", RUN+="${pkgs.duo-display}/bin/duo-display top"
+    ACTION=="remove", ATTRS{idVendor}=="0b05", ATTRS{idProduct}=="1b2c", RUN+="${pkgs.duo-display}/bin/duo-display both"
   '';
 }
