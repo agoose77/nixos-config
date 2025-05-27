1fcb6c555b943da19a6799bc84d4a0ac6524411e
Angus Hollands <goosey15@gmail.com>
2025-05-27 10:06:36 +0100

    fix: use systemd with inotify

--- hosts/waldo/displays.nix
+++ hosts/waldo/displays.nix
@@ -1,7 +1,18 @@
 {pkgs, ...}: {
   # Disable eDP-2 when keyboard plugged in
-  services.udev.extraRules = ''
-    ACTION=="add",    ATTRS{idVendor}=="0b05", ATTRS{idProduct}=="1b2c", RUN+="${pkgs.duo-display}/bin/duo-display top"
-    ACTION=="remove", ATTRS{idVendor}=="0b05", ATTRS{idProduct}=="1b2c", RUN+="${pkgs.duo-display}/bin/duo-display both"
-  '';
+  systemd.user.services.respond-to-keyboard = {
+    description = "Switch displays as keyboard is removed.";
+    wantedBy = ["multi-user.target"];
+    serviceConfig.Type = "simple";
+    script = ''
+      #!/usr/bin/env bash
+      while ${pkgs.inotify-tools}/bin/inotifywait -e attrib /dev/bus/usb/*/; do
+      if ${pkgs.usbutils}/bin/lsusb -d 0b05:1b2c; then
+      ${pkgs.hyprland}/bin/hyprctl keyword monitor "eDP-2,disable"
+      else
+      ${pkgs.hyprland}/bin/hyprctl keyword monitor "eDP-2,1920x1200@60,auto-down,1"
+      fi
+      done
+    '';
+  };
 }
