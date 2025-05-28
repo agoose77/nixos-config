b772d6b16f3c23313f75477eea65e5fcb277035b
Angus Hollands <goosey15@gmail.com>
2025-05-28 20:07:03 +0100

    fix: run at graphical-session

--- hosts/waldo/displays.nix
+++ hosts/waldo/displays.nix
@@ -2,17 +2,24 @@
   # Disable eDP-2 when keyboard plugged in
   systemd.user.services.respond-to-keyboard = {
     description = "Switch displays as keyboard is removed.";
-    wantedBy = ["multi-user.target"];
+    wantedBy = ["graphical-session.target"];
     serviceConfig.Type = "simple";
     script = ''
       #!/usr/bin/env bash
+      set -eu
+      function update_displays() {
+          if ${pkgs.usbutils}/bin/lsusb -d 0b05:1b2c; then
+              ${pkgs.hyprland}/bin/hyprctl keyword monitor "eDP-2,disable"
+          else
+              ${pkgs.hyprland}/bin/hyprctl keyword monitor "eDP-2,1920x1200@60,auto-down,1"
+          fi
+      }
+
+      update_displays
       while ${pkgs.inotify-tools}/bin/inotifywait -e attrib /dev/bus/usb/*/; do
-      if ${pkgs.usbutils}/bin/lsusb -d 0b05:1b2c; then
-      ${pkgs.hyprland}/bin/hyprctl keyword monitor "eDP-2,disable"
-      else
-      ${pkgs.hyprland}/bin/hyprctl keyword monitor "eDP-2,1920x1200@60,auto-down,1"
-      fi
+          update_displays
       done
+
     '';
   };
 }
