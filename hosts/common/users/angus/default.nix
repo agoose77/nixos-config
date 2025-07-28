df43d3464d628126b601424fbd593afd0d4c7419
Angus Hollands <goosey15@gmail.com>
2025-07-28 19:24:33 +0100

    fix: change groups

--- hosts/common/users/angus/default.nix
+++ hosts/common/users/angus/default.nix
@@ -12,7 +12,7 @@
     angus = {
       isNormalUser = true;
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["networkmanager" "wheel" "media" "docker"];
+      extraGroups = ["networkmanager" "wheel" "media" "docker" "dialout"];
       shell = pkgs.bash;
       uid = 1000;
 
