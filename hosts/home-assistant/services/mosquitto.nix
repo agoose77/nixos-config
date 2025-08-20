851a106e5294534dd26491e90fcd3c0a1cb5833f
Angus Hollands <goosey15@gmail.com>
2025-08-20 22:01:30 +0100

    feat: add mosquitto conf

--- hosts/home-assistant/services/mosquitto.nix
+++ hosts/home-assistant/services/mosquitto.nix
@@ -1,4 +1,17 @@
 {
+  environment.etc."mosquitto/mosquitto.conf".text = ''
+    allow_anonymous false
+    listener 1883
+    listener 9001
+    protocol websockets
+    persistence true
+    password_file /mosquitto/config/pwfile
+    persistence_file mosquitto.db
+    persistence_location /mosquitto/data/
+  '';
+  environment.etc."mosquitto/pwfile".text = ''
+    root:$7$101$TogZKXwkPsXXDZvE$rBRp2ORyuWGrNH5DY1QJa6+eqNwDKt0185e99TYW6p7oKksyRX/E13jEJGWO2BoUys1vSMETJXP/YIFDKiF6Eg==
+  '';
   virtualisation.oci-containers.containers.mosquitto = {
     ports = [
       "1883:1883"
