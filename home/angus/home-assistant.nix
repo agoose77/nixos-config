cc6ab3dc15b98db526552e3a4501e028feed6fd8
Angus Hollands <goosey15@gmail.com>
2025-08-20 13:06:05 +0100

    refactor: basic ha config

--- home/angus/home-assistant.nix
+++ home/angus/home-assistant.nix
@@ -1,18 +1,5 @@
-{pkgs, ...}: {
+{
   imports = [
     ./global.nix
-  ];
-  home.packages = [
-    pkgs.luminance
-    pkgs.brightnessctl
-  ];
-  monitors = [
-    {
-      name = "eDP-1";
-      width = 1920;
-      height = 1080;
-      workspace = "1";
-      primary = true;
-    }
   ];
 }
