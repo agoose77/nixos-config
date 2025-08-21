d66f1f8fe1763163f1e11e926585400266d6d098
Angus Hollands <goosey15@gmail.com>
2025-08-21 16:30:24 +0100

    fix: pub key

--- home/angus/hass.nix
+++ home/angus/hass.nix
@@ -2,6 +2,15 @@
   imports = [
     ./global.nix
   ];
+  monitors = [
+    {
+      name = "eDP-1";
+      width = 1920;
+      height = 1080;
+      workspace = "1";
+      primary = true;
+    }
+  ];
   home.packages = [
     pkgs.luminance
     pkgs.brightnessctl
