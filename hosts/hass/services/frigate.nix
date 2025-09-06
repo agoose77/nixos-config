523916c39e297fcbe0a7cc36249fe306b614f74e
Angus Hollands <goosey15@gmail.com>
2025-09-06 22:11:47 +0100

    fix: add model

--- hosts/hass/services/frigate.nix
+++ hosts/hass/services/frigate.nix
@@ -36,6 +36,14 @@
         device = "GPU";
       };
     };
+    model = {
+      width = 300;
+      height = 300;
+      input_tensor = "nhwc";
+      input_pixel_format = "bgr";
+      path = "/openvino-model/ssdlite_mobilenet_v2.xml";
+      labelmap_path = "/openvino-model/coco_91cl_bkgr.txt";
+    };
     go2rtc = {
       webrtc = {
         candidates = [
