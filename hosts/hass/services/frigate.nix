b4279f88abff143aa1f16388539f16ccd182718b
Angus Hollands <goosey15@gmail.com>
2025-09-06 22:22:28 +0100

    fix: pass d128

--- hosts/hass/services/frigate.nix
+++ hosts/hass/services/frigate.nix
@@ -430,7 +430,7 @@ in {
     image = "ghcr.io/blakeblackshear/frigate:0.16.0";
     extraOptions = [
       "--device=/dev/bus/usb"
-      "--device=/dev/dri/"
+      "--device=/dev/dri/renderD128"
       "--tmpfs=/tmp/cache:rw,size=1g,mode=1777"
       "--shm-size=256mb"
       "--network=mqtt-bridge"
