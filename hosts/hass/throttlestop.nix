59384efcb112936041f14fbba651186f8d708878
Angus Hollands <goosey15@gmail.com>
2025-09-24 20:23:47 +0100

    fix: timer

--- hosts/hass/throttlestop.nix
+++ hosts/hass/throttlestop.nix
@@ -3,21 +3,19 @@
   lib,
   ...
 }: {
-  # Track activity
   systemd.services.throttlestop = {
-    description = "Update CPU temperature and voltage targets.";
-    wantedBy = ["multiuser.target"];
-    after = ["multiuser.target"];
-    serviceConfig = {
-    Type = "simple";
-      Restart = "always";
-      RuntimeMaxSec = "30m";
-    };
+    serviceConfig.Type = "oneshot";
     script = ''
-      #!/usr/bin/env bash
-      set -eu
       ${lib.getExe pkgs.throttlestop} temperature "{\"offset\": 20}"
       ${lib.getExe pkgs.throttlestop} voltage "{\"cache\": -149, \"cpu\": -149}"
     '';
+  };
+  systemd.timers.throttlestop = {
+    wantedBy = ["timers.target"];
+    partOf = ["throttlestop.service"];
+    timerConfig = {
+      OnCalendar = "*-*-* *:30:00";
+      Unit = "throttlestop.service";
+    };
   };
 }
