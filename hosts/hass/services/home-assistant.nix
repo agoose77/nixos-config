e476331e033cb7b319f7b56698b3a3836915dc09
Angus Hollands <goosey15@gmail.com>
2025-09-30 09:46:51 +0100

    wip: define custom component versions

--- hosts/hass/services/home-assistant.nix
+++ hosts/hass/services/home-assistant.nix
@@ -1,4 +1,41 @@
-{
+{pkgs, ...}: let
+  myenergiComponent = pkgs.fetchFromGitHub {
+    owner = "CJNE";
+    repo = "ha-myenergi";
+    tag = "0.0.30";
+    hash = "sha256-vy3I0MLNErz6Y/hoH7Jv/Mpqtg4E2i8cQkGGWpTyfhs=";
+  };
+  frigateComponent = pkgs.fetchFromGitHub {
+    owner = "blakeblackshear";
+    repo = "frigate-hass-integration";
+    tag = "v5.9.4";
+    hash = "sha256-LzrIvHJMB6mFAEfKoMIs0wL+xbEjoBIx48pSEcCHmg4=";
+  };
+  climateComponent = pkgs.fetchFromGitHub {
+    owner = "jcwillox";
+    repo = "hass-template-climate";
+    tag = "v1.3.0";
+    hash = "sha256-hWYYY0kt/RfdCyNR3skiYOyyQ7KF35Xbh8NczIDzr58=";
+  };
+  octopusComponent = pkgs.fetchFromGitHub {
+    owner = "BottlecapDave";
+    repo = "HomeAssistant-OctopusEnergy";
+    tag = "v16.3.1";
+    hash = "sha256-rn8wCGUYisLgr61Cd2qaQGfSiAtjKMo2wG/AotEXknE=";
+  };
+  spookComponent = pkgs.fetchFromGitHub {
+    owner = "frenck";
+    repo = "spook";
+    tag = "v4.0.1";
+    hash = "sha256-0IihrhATgraGmuMRnrbGTUrtlXAR+CooENSIKSWIknY=";
+  };
+  tplinkComponent = pkgs.fetchFromGitHub {
+    owner = "AlexandrErohin";
+    repo = "home-assistant-tplink-router";
+    tag = "v2.9.0";
+    hash = "sha256-2yhoBIU2NMLhAvezB82/gs+A0ZVVsMenvOR1HyU1PEM=";
+  };
+in {
   # Open shelly port
   networking.firewall.allowedUDPPorts = [5683];
   networking.firewall.allowedTCPPorts = [8123];
@@ -17,6 +54,12 @@
     volumes = [
       "/run/dbus:/run/dbus:ro"
       "/etc/home-assistant:/config"
+      "${myenergiComponent}/custom_components/myenergi:/config/custom_components/myenergi"
+      "${frigateComponent}/custom_components/frigate:/config/custom_components/frigate"
+      "${climateComponent}/custom_components/climate_template:/config/custom_components/climate_template"
+      "${octopusComponent}/custom_components/octopus_energy:/config/custom_components/octopus_energy"
+      "${spookComponent}/custom_components/spook:/config/custom_components/spook"
+      "${tplinkComponent}/custom_components/tplink_router:/config/custom_components/tplink_router"
     ];
   };
 }
