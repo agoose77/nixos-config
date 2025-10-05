ed0fa876789aedc4b2316194b63ec2c45f508cee
Angus Hollands <goosey15@gmail.com>
2025-10-05 17:07:27 +0100

    fix: add jellyfin

--- hosts/hass/services/arr.nix
+++ hosts/hass/services/arr.nix
@@ -142,27 +142,26 @@
         "--network=container:gluetun"
       ];
     };
-    #     # Jellyfin
-    #     containers.jellyfin = {
-    #       environment = {
-    #         PUID = "1000";
-    #         PGID = "1000";
-    #
-    #         LIBVA_DRIVER_NAME = "i965";
-    #       };
-    #       image = "lscr.io/linuxserver/jellyfin:10.10.7";
-    #       volumes = [
-    #         "/etc/jellyfin/data:/config"
-    #         "/mnt/data/media/tv:/tv"
-    #       ];
-    #       ports = [
-    #         "8096:8096"
-    #         "7359:7359/udp"
-    #         "1900:1900/udp"
-    #       ];
-    #       extraOptions = [
-    #         "--device=/dev/dri/renderD129:/dev/dri/renderD129"
-    #       ];
-    #     };
+    # Jellyfin
+    containers.jellyfin = {
+      environment = {
+        PUID = "1000";
+        PGID = "1000";
+        LIBVA_DRIVER_NAME = "iHD";
+      };
+      image = "lscr.io/linuxserver/jellyfin:10.10.7";
+      volumes = [
+        "/etc/jellyfin/data:/config"
+        "/mnt/data/media/tv:/tv"
+      ];
+      ports = [
+        "8096:8096"
+        "7359:7359/udp"
+        "1900:1900/udp"
+      ];
+      extraOptions = [
+        "--device=/dev/dri"
+      ];
+    };
   };
 }
