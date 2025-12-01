1b1c6cc3a300395b4a72b9c5e042717104666c94
Angus Hollands <goosey15@gmail.com>
2025-12-01 11:46:59 +0000

    feat: migrate to sops

--- hosts/nixos/activity.nix
+++ hosts/nixos/activity.nix
@@ -3,6 +3,27 @@
   pkgs,
   ...
 }: {
+  sops.templates."nixos-activity.json".file = let
+    config = {
+      password = "${config.sops.placeholder.activityPassword}";
+      username = "${config.sops.placeholder.activityUser}";
+      port = 1883;
+      host = "hass.local";
+      timeout = 300;
+      interval = 30;
+      tls = false;
+      discoveryTopic = "homeassistant/binary_sensor/nixos/occupancy/config";
+      stateTopic = "nixos/occupancy/state";
+      name = "Occupancy";
+      deviceArea = "Angus' Office";
+      deviceName = "NixOS";
+      deviceIds = [
+        "nixos-idle-monitor"
+      ];
+    };
+  in
+    ((pkgs.formats.json {}).generate "nixos-activity.json" config).path;
+
   # Track activity
   systemd.user.services.report-activity = {
     description = "Report activity to MQTT server.";
