71336f8ec42a48881c8fb7b28b10c9e41d1b67f2
Angus Hollands <goosey15@gmail.com>
2025-12-02 12:17:28 +0000

    refactor: nix

--- hosts/common/features/nix.nix
+++ hosts/common/features/nix.nix
@@ -6,7 +6,6 @@
 }: {
   # This will additionally add your inputs to the system's legacy channels
   # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = ["/etc/nix/path"];
   environment.etc =
     lib.mapAttrs'
     (name: value: {
@@ -14,20 +13,23 @@
       value.source = value.flake;
     })
     config.nix.registry;
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-  nix.settings = {
-    # Enable flakes and new 'nix' command
-    experimental-features = "nix-command flakes";
-    # Deduplicate and optimize nix store
-    auto-optimise-store = true;
-  };
+  nix = {
+    nixPath = ["/etc/nix/path"];
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+    };
 
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    # Keep the last 3 generations
-    options = "--delete-older-than +3";
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      # Keep the last 3 generations
+      options = "--delete-older-than +3";
+    };
   };
 }
