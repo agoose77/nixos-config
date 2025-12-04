2dc20ed2fca2324ba5242fbfdf4cdee25a9db82a
Angus Hollands <goosey15@gmail.com>
2025-12-04 09:30:35 +0000

    feat: add niri display package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,7 @@ pkgs: {
   nvidia-offload = pkgs.callPackage ./nvidia-offload {};
   idle-monitor = pkgs.callPackage ./idle-monitor {};
   duo-display-hyprland = pkgs.callPackage ./duo-display-hyprland {};
+  duo-display-niri = pkgs.callPackage ./duo-display-niri {};
   myst = pkgs.callPackage ./myst {};
   throttlestop = pkgs.callPackage ./throttlestop {};
 }
