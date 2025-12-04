c2dd9fd448d506826c9f37235f36fc5e61508c66
Angus Hollands <goosey15@gmail.com>
2025-12-04 07:55:52 +0000

    refactor: add package name

--- hosts/waldo/displays.nix
+++ hosts/waldo/displays.nix
@@ -2,13 +2,12 @@
   environment.systemPackages = [
     pkgs.acpi
     pkgs.brightnessctl
-    pkgs.duo-display
     # For pulling Bluetooth keys from Windows
     pkgs.chntpw
     pkgs.dislocker
   ];
   services.zenbook-display = {
     enable = true;
-    package = pkgs.duo-display;
+    package = pkgs.duo-display-hyprland;
   };
 }
