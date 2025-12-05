80a058626425fd5397ad46dbac298d94fe19e84e
Angus Hollands <goosey15@gmail.com>
2025-12-05 17:08:43 +0000

    wip: home module for niri

--- flake.nix
+++ flake.nix
@@ -2,28 +2,23 @@
   description = "Angus' NixOS configuration";
 
   inputs = {
-    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # neovim
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs"; # keep nixvim nixpkgs consistent with nixpkgs
     };
 
-    # Home manager
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # lanzaboote
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # lanzaboote
     stylix = {
       url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -31,6 +26,11 @@
 
     sops-nix = {
       url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    niri-flake = {
+      url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -41,6 +41,8 @@
     home-manager,
     lanzaboote,
     stylix,
+    sops-nix,
+    niri-flake,
     ...
   } @ inputs: let
     inherit (self) outputs;
