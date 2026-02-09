26a25557593514de59acff8b44a141a17535a73a
Angus Hollands <goosey15@gmail.com>
2026-02-09 11:10:16 +0000

    feat: update HA

--- hosts/hass/services/home-assistant.nix
+++ hosts/hass/services/home-assistant.nix
@@ -87,7 +87,7 @@ in {
       environment.TZ = "Europe/London";
       # This fixes a bug
       environment.PYTHONPATH = "/usr/local/lib/python3.13:/config/deps";
-      image = "ghcr.io/home-assistant/home-assistant:2025.12.5"; # Warning: if the tag does not change, the image will not be updated
+      image = "ghcr.io/home-assistant/home-assistant:2026.2 "; # Warning: if the tag does not change, the image will not be updated
       extraOptions =
         [
           "--network=host"
