1036d04e608339bfbe135fb2148817b1b2f0784d
Angus Hollands <goosey15@gmail.com>
2026-03-09 11:49:14 +0000

    fix: try fixing npu

--- modules/hosts/waldo/configuration.nix
+++ modules/hosts/waldo/configuration.nix
@@ -11,18 +11,25 @@
     ];
     boot.lanzaboote.configurationLimit = 2;
 
+    networking.hostName = "waldo";
+
+    environment.sessionVariables = {
+      WLR_NO_HARDWARE_CURSORS = "1";
+      NIXOS_OZONE_WL = "1"; # Enable Wayland support for slack
+    };
+
+    environment.systemPackages = [
+      pkgs.acpi
+      # For pulling Bluetooth keys from Windows
+      pkgs.chntpw
+      pkgs.dislocker
+    ];
+
     services.hardware.bolt.enable = true;
     services.zenbook-display = {
       enable = true;
       package = pkgs.local.duo-display-niri;
     };
-
-    swapDevices = [
-      {
-        device = "/swapfile";
-        size = 16 * 1024; # 16GB
-      }
-    ];
   };
   flake.modules.homeManager.niri = {pkgs, ...}: {
     home.packages = [
--- modules/hosts/waldo/hardware.nix
+++ modules/hosts/waldo/hardware.nix
@@ -1,16 +1,24 @@
 {lib, ...}: {
   flake.modules.nixos.waldo = {
+    pkgs,
+    modulesPath,
+    ...
+  }: {
+    # FIXME: Fix for missing NPU option
+    imports = [(modulesPath + "/hardware/cpu/intel-npu.nix")];
     boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "vmd" "nvme" "usbhid" "usb_storage" "sd_mod"];
     boot.initrd.kernelModules = [];
-    boot.kernelModules = ["kvm-intel" "intel-vpu"];
+    boot.kernelModules = ["kvm-intel"];
     boot.extraModulePackages = [];
 
     boot.initrd.luks.devices."luks-e9d523c6-75c0-4a2c-88f9-981e552b7519".device = "/dev/disk/by-uuid/e9d523c6-75c0-4a2c-88f9-981e552b7519";
 
-    swapDevices = [];
-
-    hardware.enableRedistributableFirmware = lib.mkDefault true;
-    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+    swapDevices = [
+      {
+        device = "/swapfile";
+        size = 16 * 1024; # 16GB
+      }
+    ]; # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
     # (the default) this is the recommended approach. When using systemd-networkd it's
     # still possible to use this option, but it's recommended to use it in conjunction
     # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
@@ -18,7 +26,9 @@
     # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
     nixpkgs.hostPlatform = "x86_64-linux";
+    hardware.enableRedistributableFirmware = lib.mkDefault true;
     hardware.cpu.intel.updateMicrocode = true;
+    hardware.cpu.intel.npu.enable = true;
 
     hardware.graphics = {
       enable = true;
