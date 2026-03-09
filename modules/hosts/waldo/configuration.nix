71b1add34bdd195dd419d5f9815151197d52049b
Angus Hollands <goosey15@gmail.com>
2026-03-09 07:37:35 +0000

    fix: add more hardware config

--- modules/hosts/waldo/configuration.nix
+++ modules/hosts/waldo/configuration.nix
@@ -1,9 +1,32 @@
 {inputs, ...}: {
-  flake.modules.nixos.waldo = {
+  flake.modules.nixos.waldo = {pkgs, ...}: {
     imports = with inputs.self.modules.nixos; [
       system-default
       secure-boot
       bluetooth
+    ];
+    boot.kernelModules = ["intel_vpu"];
+    boot.lanzaboote.configurationLimit = 1;
+
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+      # For better video playback
+      extraPackages = with pkgs; [
+        vpl-gpu-rt
+        intel-media-driver
+        intel-vaapi-driver
+        libva-vdpau-driver
+        libvdpau-va-gl
+      ];
+    };
+    services.hardware.bolt.enable = true;
+
+    swapDevices = [
+      {
+        device = "/swapfile";
+        size = 16 * 1024; # 16GB
+      }
     ];
   };
   flake.modules.homeManager.niri = {
