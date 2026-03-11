00fec18cc816dd80b1dc03c85565451d11360f8d
Angus Hollands <goosey15@gmail.com>
2026-03-11 09:31:50 +0000

    feat: add SSH identity agent via 1password

--- modules/programs/1password.nix
+++ modules/programs/1password.nix
@@ -1,14 +1,23 @@
 {
-  flake.modules.nixos._1password = {
-    # Enable the 1Password CLI, this also enables a SGUID wrapper so the CLI can authorize against the GUI app
-    programs._1password = {
-      enable = true;
-    };
+  flake.modules = {
+    nixos._1password = {
+      # Enable the 1Password CLI, this also enables a SGUID wrapper so the CLI can authorize against the GUI app
+      programs._1password = {
+        enable = true;
+      };
 
-    # Enable the 1Password GUI with myself as an authorized user for polkit
-    programs._1password-gui = {
-      enable = true;
-      polkitPolicyOwners = ["angus"];
+      # Enable the 1Password GUI with myself as an authorized user for polkit
+      programs._1password-gui = {
+        enable = true;
+        polkitPolicyOwners = ["angus"];
+      };
+    };
+    homeManager._1password = {
+      pkgs,
+      lib,
+      ...
+    }: {
+      programs.ssh.extraOptionOverrides.IdentityAgent = "~/.1password/agent.sock";
     };
   };
 }
