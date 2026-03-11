216877968666014e0fc8ebd93b2d94ee253c6102
Angus Hollands <goosey15@gmail.com>
2026-03-11 09:14:33 +0000

    feat: enable home-manager program

--- modules/nix/tools/home-manager/home-manager.nix
+++ modules/nix/tools/home-manager/home-manager.nix
@@ -1,14 +1,19 @@
 {inputs, ...}: {
-  flake.modules.nixos.home-manager = {
-    imports = [
-      inputs.home-manager.nixosModules.home-manager
-      ({lib, ...}: {
-        home-manager = {
-          verbose = true;
-          useUserPackages = true;
-          useGlobalPkgs = true;
-        };
-      })
-    ];
+  flake.modules = {
+    nixos.home-manager = {
+      imports = [
+        inputs.home-manager.nixosModules.home-manager
+        ({lib, ...}: {
+          home-manager = {
+            verbose = true;
+            useUserPackages = true;
+            useGlobalPkgs = true;
+          };
+        })
+      ];
+    };
+  };
+  homeManager.home-manager = {
+    programs.home-manager.enable = true;
   };
 }
