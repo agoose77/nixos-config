72a81a82ff156c611a35b6eb98acc5ef4d60e94d
Angus Hollands <goosey15@gmail.com>
2026-04-01 11:23:09 +0100

    feat: bump HA, fix theme

--- modules/hosts/hass/services/home-assistant.nix
+++ modules/hosts/hass/services/home-assistant.nix
@@ -103,7 +103,7 @@
         environment.TZ = "Europe/London";
         # This fixes a bug
         environment.PYTHONPATH = "/usr/local/lib/python3.13:/config/deps";
-        image = "ghcr.io/home-assistant/home-assistant:2026.2.3 "; # Warning: if the tag does not change, the image will not be updated
+        image = "ghcr.io/home-assistant/home-assistant:2026.3.4 "; # Warning: if the tag does not change, the image will not be updated
         extraOptions =
           [
             "--network=host"
@@ -127,7 +127,7 @@
             "${webResources.miniGraphCard}:/config/www/miniGraphCard/mini-graph-card-bundle.js"
             "${webResources.materialYouTheme}:/config/www/materialYouTheme/"
             "${webResources.materialYouUtilities}:/config/www/materialYouUtilities/"
-            "${webResources.materialYouTheme}/themes/material_you.yaml:/config/themes/"
+            "${webResources.materialYouTheme}/themes/material_you.yaml:/config/themes/material_you.yaml"
           ];
       };
       matter-server = {
