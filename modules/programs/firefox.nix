c8eed00258dffadf7f10b120e880a6155c1d8602
Angus Hollands <goosey15@gmail.com>
2026-05-08 12:38:25 +0100

    feat: add profile to ffox for lang

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -1,12 +1,19 @@
-{flake.modules.homeManager.firefox = {
-  lib,
-  config,
-  ...
-}: {
-  programs.firefox = {
-    enable = true;
-    profiles.default = {
+{
+  flake.modules.homeManager.firefox = {
+    lib,
+    config,
+    ...
+  }: {
+    programs.firefox = {
+      enable = true;
+      profiles.default = {
+      };
+      languagePacks = ["en-GB"];
+      policies = {
+        "RequestedLocales" = ["en-GB"];
+        "AIControls.Default.Value" = "blocked";
+      };
     };
+    stylix.targets.firefox.profileNames = lib.mkIf config.stylix.enable ["default"];
   };
-  stylix.targets.firefox.profileNames = lib.mkIf config.stylix.enable ["default"];
-};}
+}
