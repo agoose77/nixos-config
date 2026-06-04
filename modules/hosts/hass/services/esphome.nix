7886eecdce8404e7ba326373807f84660b104d88
Angus Hollands <goosey15@gmail.com>
2026-06-04 15:25:27 +0100

    feat: support multiple devices

--- modules/hosts/hass/services/esphome.nix
+++ modules/hosts/hass/services/esphome.nix
@@ -4,7 +4,34 @@
     pkgs,
     lib,
     ...
-  }: {
+  }: let
+    deviceNames = {
+      living-room = "Living Room";
+      kitchen = "Kitchen";
+    };
+    baseESPConfig = {
+      esp32 = {
+        board = "m5stack-atom";
+        framework = {
+          type = "arduino";
+        };
+      };
+      logger = null;
+      api = null;
+      ota = [
+        {
+          platform = "esphome";
+        }
+      ];
+      wifi = {
+        ssid = config.sops.placeholder.wifi-ssid;
+        password = config.sops.placeholder.wifi-password;
+      };
+      bluetooth_proxy = {
+        active = true;
+      };
+    };
+  in {
     sops.secrets = {
       wifi-ssid = {
         sopsFile = ../secrets.yaml;
@@ -19,45 +46,26 @@
         key = "wifi-password";
       };
     };
-    sops.templates."living-room.yaml".file = let
-      livingRoomConfig = {
-        esphome = {
-          name = "esphome-web-fa8368";
-          friendly_name = "Living Room ESP";
-          min_version = "2025.11.0";
-          name_add_mac_suffix = false;
-        };
-        esp32 = {
-          board = "m5stack-atom";
-          framework = {
-            type = "arduino";
-          };
-        };
-        logger = null;
-        api = null;
-        ota = [
-          {
-            platform = "esphome";
-          }
-        ];
-        wifi = {
-          ssid = config.sops.placeholder.wifi-ssid;
-          password = config.sops.placeholder.wifi-password;
-        };
-        bluetooth_proxy = {
-          active = true;
-        };
-      };
-    in ((pkgs.formats.yaml {}).generate "living-room.yaml" livingRoomConfig);
+    sops.templates =
+      lib.mapAttrs (
+        name: display: ((pkgs.formats.yaml {}).generate "${name}.yaml" baseESPConfig
+          // {
+            esphome = {
+              name = "esphome-web-fa8368";
+              friendly_name = "${display} ESP";
+              min_version = "2025.11.0";
+              name_add_mac_suffix = false;
+            };
+          })
+      )
+      deviceNames;
 
     virtualisation.oci-containers.containers.esphome = {
       ports = [
         "6052:6052"
       ];
       image = "ghcr.io/esphome/esphome:2026.5.2";
-      volumes = [
-        "${config.sops.templates."living-room.yaml".path}:/config/esp-living-room.yaml"
-      ];
+      volumes = lib.mapAttrsToList (name: value: "${config.sops.templates."${name}".path}:/config/${name}.yaml") deviceNames;
     };
   };
 }
