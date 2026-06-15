744a8e383e1836045bf69905f52864a41a62e1e4
Angus Hollands <goosey15@gmail.com>
2026-06-15 11:27:28 +0200

    wip: disable kitty autoreload for now
    It spins up too many IO listeners

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -1,6 +1,8 @@
 {flake.modules.homeManager.kitty = {...}: {
   programs.kitty = {
     enable = true;
+    # Temporary fix for https://github.com/kovidgoyal/kitty/issues/10102
+    settings.auto_reload_config = -1;
   };
   stylix.targets.kitty.enable = true;
 };}
