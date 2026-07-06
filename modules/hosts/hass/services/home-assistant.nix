8948bff06fc9db4af558558a5cbe2f063685bb60
Angus Hollands <goosey15@gmail.com>
2026-07-06 22:19:14 +0100

    feat: bump tuya local

--- modules/hosts/hass/services/home-assistant.nix
+++ modules/hosts/hass/services/home-assistant.nix
@@ -45,8 +45,8 @@
       tuya_local = pkgs.fetchFromGitHub {
         owner = "make-all";
         repo = "tuya-local";
-        tag = "2026.6.2";
-        hash = "sha256-4897QCslPQ/5Rt1U6EcapUav7XMa65i+5aXbMyv9mxE=";
+        tag = "2026.6.4";
+        hash = "sha256-VJ2c8wegviirCT+hygg9LKPN5ZLqxfkQl6zayaXZRU0=";
       };
       spook = let
         version = "4.0.1";
