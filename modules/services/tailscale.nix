d6d4fcf20e4d2b3482197e4894501180fd460d68
Angus Hollands <goosey15@gmail.com>
2026-07-22 09:13:56 +0100

    feat: restart tailscale after suspend

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -1,12 +1,32 @@
 {
   flake.modules.nixos.tailscale =
     # Set basic bootloader options
-    {config, ...}: {
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }: {
       services.tailscale = {
         enable = true;
         port = 12345;
       };
 
       networking.firewall.allowedUDPPorts = [config.services.tailscale.port];
+
+      systemd.services.restart-tailscale-on-resume = {
+        description = "Restart Tailscale after suspend.";
+        wantedBy = ["suspend.target"];
+        after = ["suspend.target"];
+        serviceConfig = {
+          Type = "simple";
+          Restart = "on-failure";
+        };
+        script = ''
+          #!/usr/bin/env bash
+          set -eu
+          ${lib.getExe' pkgs.systemd "systemctl"} --no-block restart tailscaled.service
+        '';
+      };
     };
 }
