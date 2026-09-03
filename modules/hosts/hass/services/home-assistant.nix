257d67e952ad298e74e5808ae7e26d15e937766d
Angus Hollands <goosey15@gmail.com>
2026-09-03 21:31:04 +0100

    feat: bump hass again

--- modules/hosts/hass/services/home-assistant.nix
+++ modules/hosts/hass/services/home-assistant.nix
@@ -115,7 +115,7 @@
         environment.TZ = "Europe/London";
         # This fixes a bug
         environment.PYTHONPATH = "/usr/local/lib/python3.13:/config/deps";
-        image = "ghcr.io/home-assistant/home-assistant:2026.8.0 "; # Warning: if the tag does not change, the image will not be updated
+        image = "ghcr.io/home-assistant/home-assistant:2026.9.0 "; # Warning: if the tag does not change, the image will not be updated
         extraOptions =
           [
             "--network=host"
